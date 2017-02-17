17f7748506d7064192f7e02080efa88887163be8
Eamon Ford <eamonford@gmail.com>
2017-02-16 22:48:35 -0600

    write TODOs

---
+++ main.py
@@ -0,0 +1,8 @@
+def main():
+    print("hello world")
+    # TODO: every 15 minutes, get all rules from postgres
+    # TODO: for each rule that has status "training", check if it's been one week.
+    # TODO: if it's been one week, get the average moisture for that sensor for the past week
+    # TODO: store this average moisture value as the rule's 'threshold' property
+
+main()
